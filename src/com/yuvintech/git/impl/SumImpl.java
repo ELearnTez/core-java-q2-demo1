d5b8f55f0e51c1319c12ff784760a221ce0d9e25
Harish <Harish@DESKTOP-2AUTKI4>
2020-08-02 10:49:27 +0530

    sum commit

---
+++ src/com/yuvintech/git/impl/SumImpl.java
@@ -0,0 +1,13 @@
+package com.yuvintech.git.impl;
+
+import com.yuvintech.git.ISum;
+
+public class SumImpl implements ISum {
+
+	@Override
+	public int add(int a, int b) {
+		
+		return a + b;
+	}
+
+}
