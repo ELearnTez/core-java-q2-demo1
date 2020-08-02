780e17cb257883e6c336911781bfac47c6cf0ba1
Stratton <strattonmmei8@gmail.com>
2020-08-02 07:26:26 +0300

    substraction commit

---
+++ src/com/yuvintech/git/impl/SubtractionImpl.java
@@ -0,0 +1,13 @@
+package com.yuvintech.git.impl;
+
+import com.yuvintech.git.ISubtraction;
+
+public class SubtractionImpl implements ISubtraction {
+
+	@Override
+	public int subtract(int a, int b) {
+		
+		return a - b;
+	}
+
+}
