d713649ffc6646a0777f65c198255c8b886ee176
hp <hp@hp-PC>
2020-08-02 10:46:54 +0530

    multiplication

---
+++ src/com/yuvintech/git/impl/MultiImpl.java
@@ -0,0 +1,13 @@
+package com.yuvintech.git.impl;
+
+import com.yuvintech.git.IMultiplicate;
+
+public class MultiImpl implements IMultiplicate {
+
+	@Override
+	public int multiplication(int a, int b) {
+		
+		return a*b;
+	}
+
+}
