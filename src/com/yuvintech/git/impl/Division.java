7299ab316ae3d5de4a8c2b3ba33534a8f8bbda67
chinteshkumar <52699016+chinteshkumar@users.noreply.github.com>
2020-08-02 11:01:18 +0530

    Chintesh Changes

---
+++ src/com/yuvintech/git/impl/Division.java
@@ -0,0 +1,17 @@
+package com.yuvintech.git.impl;
+import com.yuvintech.git.Divide;
+
+public class Division implements Divide  {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int divide(int a, int b) {
+		// TODO Auto-generated method stub
+		return a/b;
+	}
+
+}
